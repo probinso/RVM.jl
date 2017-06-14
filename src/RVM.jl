d6426a9b94e5f6b6ec68bcf6798fe5f2180d1db6
probinso <pmoss.robinson@gmail.com>
2017-06-13 22:41:47 -0700

    rename

--- src/RVM.jl
+++ src/RVM.jl
@@ -1,4 +1,4 @@
-module SRM
+module RVM
 export hello
 
 using MLBase
@@ -7,5 +7,25 @@ using MLKernels
 function hello(name)
     "Hello, $(name)!"
 end
+
+#=
+    def __init__(
+        self,
+        kernel='rbf',
+        degree=3,
+        coef1=None,
+        coef0=0.0,
+        n_iter=3000,
+        tol=1e-3,
+        alpha=1e-6,
+        threshold_alpha=1e9,
+        beta=1.e-6,
+        beta_fixed=False,
+        bias_used=True,
+        verbose=False
+    ):
+=#
+
+
 
 end
