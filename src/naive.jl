842c503186a20d071c88801c2af3afab19a2d209
probinso <pmoss.robinson@gmail.com>
2017-06-16 20:13:44 -0700

    yup

--- src/naive.jl
+++ src/naive.jl
@@ -4,11 +4,16 @@ using MLKernels
 using Compat
 using DataFrames
 
+__precompile__()
+
+@inline expit(x) = 1/(1+exp(-x))
+
 type RVMSpec
     kernel::MLKernels.Kernel
     n_iter::Integer
     normalizer::Function
     threshold::Real
+    tol::Real
 end
 
 type RVMFit{R <: Real}
@@ -20,7 +25,6 @@ type RVMFit{R <: Real}
     w::Vector{R}
 end
 
-
 function whitening(Obs::AbstractMatrix)
     μ = mean(Obs, 1)
     σ = var(Obs, 1)
@@ -28,8 +32,7 @@ function whitening(Obs::AbstractMatrix)
     f(M) = (M .- μ) ./ σ
 end
 
-
-function get_data(ifname, target)
+function get_data(ifname::AbstractString, target::Symbol)
     data   = DataFrames.readtable(ifname)
 
     Obs  = convert(Matrix{Float64},
@@ -38,82 +41,124 @@ function get_data(ifname, target)
     Obs, ObsT
 end
 
-#predict(model, Test)
-
 function fit{R <: Real}(S::RVMSpec, Obs::AbstractMatrix{R}, ObsT::AbstractVector)
-
     # sample count, features
     n, p = size(Obs)
 
+    # standardize normalizing process
     normalize = S.normalizer(Obs)
-    X = normalize(Obs)
+    X  = normalize(Obs)
 
+    # Relevance Vectors
+    RV = X
+
+    # standardize encoding process
     lm = labelmap(ObsT)
     encode(V) = labelencode(lm, V)
-    T  = encode(ObsT)
+    t  = encode(ObsT)
 
-    ϕ = MLKernels.kernelmatrix(S.kernel, X, X)
-
+    # design matrix / basis functions
+    _ = MLKernels.kernelmatrix(S.kernel, X, X)
+    ϕ = [_ ones(size(_, 1))] # augment with bias
     basis_count = size(ϕ, 2)
-    α = ones(basis_count) ./ basis_count
-    β = 1e-6
 
-    m = zeros(basis_count)
+    # initialize priors
+    α₀ = ones(basis_count) .* 1e-6
+    α  = α₀[1:end]
+    w  = zeros(basis_count)
 
-    w = zeros(p)
+    # reporting information
+    steps = S.n_iter
+    succ  = false
 
-    modelfit::RVMFit{R} = RVMFit(S.kernel, normalize, encode, false, 0, zeros(p))
+    for i in 1:S.n_iter
+        println(size(α))
+
+        # update values
+        w, Σ = _posterior(w, α, ϕ, t)
+        γ = 1 .- α .* diag(Σ)
+        α = γ ./ (w .^ 2)
+
+        # identify informative vectors
+        keep = α .< S.threshold
+        if !any(keep)
+            keep[1] = true
+        end
+        keep[end] = true
+
+        # downselect uninformative vectors
+        α₀ = α₀[keep]
+        α  = α[keep]
+        γ  = γ[keep]
+        ϕ  = ϕ[:, keep]
+        Σ  = Σ[keep, keep]
+        w  = w[keep]
+        RV = RV[keep[1:end-1], :]
+
+        # 
+        if maximum(abs(α .- α₀)) < S.tol
+            steps = i
+            succ  = true
+            break
+        end
+        α₀ = α[1:end]
+    end
+
+    modelfit::RVMFit{R} = RVMFit(S.kernel, normalize, encode, succ, steps, w)
 end
 
-function _classify(X::AbstractMatrix, ϕ::AbstractMatrix)
-    Compat.logistic(ϕ *  X)
+function _classify(w::AbstractVector, ϕ::AbstractMatrix)
+    [expit(x) for x in ϕ * w]
 end
 
-function _log_posterior(X::AbstractMatrix, α::AbstractVector, ϕ::AbstractMatrix, t::AbstractVector)
+function _log_posterior(w::AbstractMatrix, α::AbstractVector,
+                        ϕ::AbstractMatrix, t::AbstractVector)
     A = diagm(α)
-    y = _classify(X, ϕ)
-    log_p = 0.5 * X' * A * X - sum(log([ y[t .== 1] (1 - y[t .== 2]) ]))
-    jacobian = A * X - ϕ' * (t - y)
+    y = _classify(w, ϕ)
+    log_p = 0.5 * m' * A * w - sum(log([ y[t .== 1] (1 - y[t .== 2]) ]))
+    jacobian = A * w - ϕ' * (t - y)
     log_p, jacobian
 end
 
-function _hessian(X::AbstractMatrix, α::AbstractVector, ϕ::AbstractMatrix, t::AbstractVector)
+function _hessian(w::AbstractVector, α::AbstractVector,
+                  ϕ::AbstractMatrix, t::AbstractVector)
     A = diagm(α)
-    y = _classify(X, ϕ)
-    B = diagm(y * (1 - y))
+    y = _classify(w, ϕ)
+    B = diagm(y .* (1 - y))
 
     #∇(w) = ϕ' * (t - y) - A * w
     H = -1 *  (ϕ' * B * ϕ + A)
 end
 
-    function _posterior(X::AbstractMatrix, α::AbstractVector, ϕ::AbstractMatrix, t::AbstractVector)
-    log_p, jacobian = _log_posterior(X, α, ϕ, t)
-
-
+function _posterior(w::AbstractVector, α::AbstractVector,
+                    ϕ::AbstractMatrix, t::AbstractVector)
+    _log_posterior
+    _hessian
+    #(w, α, ϕ, t)
+    w = w #... # minimize weights with fixed priors
+    Σ = inv(_hessian(w, α, ϕ, t))
+    w, Σ
+end
 
 #=
 def _log_posterior(self, m, alpha, phi, t):
 
-        y = self._classify(m, phi)
+        y = self._classify(w, phi)
 
         log_p = -1 * (np.sum(np.log(y[t == 1]), 0) +
                       np.sum(np.log(1-y[t == 0]), 0))
-        log_p = log_p + 0.5*np.dot(m.T, np.dot(np.diag(alpha), m))
+        log_p = log_p + 0.5*np.dot(w.T, np.dot(np.diag(alpha), m))
 
         jacobian = np.dot(np.diag(alpha), m) - np.dot(phi.T, (t-y))
 
         return log_p, jacobian
 =#
 
-
-
 Train, TrainT = get_data("../data/training.csv", :class)
-spec  = RVMSpec(MLKernels.GaussianKernel(), 10, whitening, 1e-3)
+spec  = RVMSpec(MLKernels.GaussianKernel(), 10, whitening, 1e9, 1e-3)
 model = fit(spec, Train, TrainT)
 
 #Test, TestT = get_data("../data/testing.csv", :class)
-
-
 
 #=
 function prune(X, α, ϕ, σ, thresh)
@@ -122,7 +167,7 @@ function prune(X, α, ϕ, σ, thresh)
 
 
 function fit{R <: Real}
-    (M::RVMSpec, Obs::AbstractMatrix{R}, ObsT::AbstractVector)
+    (w::RVMSpec, Obs::AbstractMatrix{R}, ObsT::AbstractVector)
 
     # sample count, features
     n, p = size(Obs)
@@ -134,13 +179,13 @@ function fit{R <: Real}
     encode(V) = labelencode(lm, V)
     T  = encode(ObsT)
 
-    ϕ = MLKernels.kernelmatrix(M.kernel, X, X)
+    ϕ = MLKernels.kernelmatrix(w.kernel, X, X)
 
     basis_count = size(ϕ, 2)
     α = ones(basis_count) ./ basis_count
     β = 1e-6
 
-    m = zeros(basis_count)
+    w = zeros(basis_count)
 
     for i in 1:M.n_iter
 
